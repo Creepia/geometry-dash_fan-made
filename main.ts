a961d675a82fb44097d8baf0cf5221871115170d
Creepia <63631846+Creepia@users.noreply.github.com>
2020-11-01 13:15:20 +0800

    added the item

--- main.ts
+++ main.ts
@@ -1,8 +1,20 @@
+scene.onOverlapTile(SpriteKind.Player, myTiles.tile2, function (sprite, location) {
+    if (controller.A.isPressed()) {
+        gra = gra==1?0:1
+tiles.setTileAt(location, myTiles.transparency16)
+        spider.startEffect(effects.ashes, 200)
+    }
+})
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (spider.isHittingTile(CollisionDirection.Top) || spider.isHittingTile(CollisionDirection.Bottom)) {
         gra = gra==1?0:1
+IsOnWall = 1
+        timer.after(100, function () {
+            IsOnWall = 0
+        })
     }
 })
+let IsOnWall = 0
 let spider: Sprite = null
 let gra = 0
 let level = [0, 0]
@@ -28,9 +40,20 @@ controller.moveSprite(spider)
 gra = 0
 tiles.setTilemap(tilemap`level_0`)
 game.onUpdate(function () {
-    if (level[0] == 0) {
-        spider.setPosition(0, 60)
-        level[0] = level[0] + 1
+    scene.centerCameraAt(spider.x, spider.y)
+    spider.vx = 120
+    if (IsOnWall == 0) {
+        if (gra == 0) {
+            spider.vy = 130
+        } else {
+            spider.vy = -150
+        }
+    } else {
+        if (gra == 0) {
+            spider.vy = 9999
+        } else {
+            spider.vy = -9999
+        }
     }
 })
 game.onUpdate(function () {
@@ -75,11 +98,8 @@ game.onUpdate(function () {
     }
 })
 game.onUpdate(function () {
-    scene.centerCameraAt(spider.x, spider.y)
-    spider.vx = 120
-    if (gra == 0) {
-        spider.vy = 150
-    } else {
-        spider.vy = -150
+    if (level[0] == 0) {
+        spider.setPosition(0, 77)
+        level[0] = level[0] + 1
     }
 })
