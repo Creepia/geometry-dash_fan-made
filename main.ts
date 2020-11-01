c44c1cd08658204566242edb59cf37bcc9f434bd
Creepia <63631846+Creepia@users.noreply.github.com>
2020-11-01 11:02:09 +0800

    two shapes done

--- main.ts
+++ main.ts
@@ -25,23 +25,6 @@ controller.moveSprite(spider)
 gra = 0
 tiles.setTilemap(tilemap`level_0`)
 game.onUpdate(function () {
-    scene.centerCameraAt(spider.x, spider.y)
-    spider.vx = 60
-    if (gra == 0) {
-        if (spider.tileKindAt(TileDirection.Bottom, myTiles.tile1)) {
-            spider.vy = 0
-        } else {
-            spider.vy = 80
-        }
-    } else {
-        if (spider.tileKindAt(TileDirection.Top, myTiles.tile1)) {
-            spider.vy = 0
-        } else {
-            spider.vy = -80
-        }
-    }
-})
-game.onUpdate(function () {
     if (level[0] == 0) {
         spider.setPosition(0, 60)
         level[0] = level[0] + 1
@@ -68,6 +51,40 @@ game.onUpdate(function () {
             . . . . . . . . . . . . . . . . 
             `)
     } else {
-    	
+        spider.setImage(img`
+            . . . . . . . . . . . . . . . . 
+            . . f f f . . . f f . . . f . . 
+            f f 4 4 2 f . f 4 4 f . f 4 f . 
+            f 2 f 4 2 f . f 4 4 f . f 4 f . 
+            f 2 f 4 2 2 f f 4 4 f f 2 4 f . 
+            2 4 f 2 2 f f f f 2 2 2 4 f . . 
+            f 2 4 f 2 2 f 4 4 4 f 2 f f . . 
+            . f 4 4 f f 2 4 4 4 2 f . . . . 
+            . . f f 2 2 f 4 4 4 2 2 f . . . 
+            . . . . f f 2 4 4 f f 2 f . . . 
+            . . . . . f 2 4 f 7 7 f 2 f . . 
+            . . . . f 2 2 4 f 7 7 f 2 f . . 
+            . . . f 2 2 2 4 4 f f 2 2 4 f . 
+            . . . f f f f f f f f f f f f . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `)
+    }
+})
+game.onUpdate(function () {
+    scene.centerCameraAt(spider.x, spider.y)
+    spider.vx = 60
+    if (gra == 0) {
+        if (spider.tileKindAt(TileDirection.Bottom, myTiles.tile1)) {
+            spider.vy = 0
+        } else {
+            spider.vy = 80
+        }
+    } else {
+        if (spider.tileKindAt(TileDirection.Top, myTiles.tile1)) {
+            spider.vy = 0
+        } else {
+            spider.vy = -80
+        }
     }
 })
